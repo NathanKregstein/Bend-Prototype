5326f4b3b6f625e070acf46927f9bcab9fb0eabc
Nathan Kregstein <nathankregstein@gmail.com>
2019-02-27 10:14:23 -0700

    Game working with serial

---
+++ src/Game/Player.js
@@ -0,0 +1,83 @@
+const Phaser = require('phaser');
+
+class Player {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.radius = 20; // radius used for collision detection
+
+    // movement
+    this.moveSpeed = 100;
+    this.forwardRot = 0;
+    this.rotSpeed = 1;
+    this.cannonRot = 0;
+    this.cannonBod = new Phaser.Geom.Rectangle(-5, 0, 10, 25)
+
+    // Geometry used for rendering
+    this.baseGeo = [
+      new Phaser.Geom.Point(-17, 10),
+      new Phaser.Geom.Point(-8, 20),
+      new Phaser.Geom.Point(8, 20),
+      new Phaser.Geom.Point(17, 10),
+      new Phaser.Geom.Point(17, -20),
+      new Phaser.Geom.Point(-17, -20),
+      new Phaser.Geom.Point(-17, 10),
+    ];
+  }
+
+  setX(newX) {
+    this.x = newX;
+  }
+  
+  setY(newY) {
+    this.y = newY;
+  }
+
+  update(deltaTime, keys) {
+    // Player Movement
+    if (keys.left.isDown) {
+      this.forwardRot -= this.rotSpeed * deltaTime / 1000
+    }
+    else if (keys.right.isDown) {
+      this.forwardRot += this.rotSpeed * deltaTime / 1000
+    }
+    if(keys.a.isDown){
+      this.cannonRot -= this.rotSpeed * deltaTime /1000
+    }
+    else if(keys.d.isDown){
+      this.cannonRot += this.rotSpeed * deltaTime /1000
+    }
+
+    // Calculate forward vector
+    const forwardX = -Math.sin(this.forwardRot);
+    const forwardY = Math.cos(this.forwardRot);
+    
+    //calculate cannon foraward vector
+    const cannonForwardX = -Math.sin(this.cannonRot);
+    const cannonForwardY = Math.cos(this.cannonRot);
+
+    if (keys.up.isDown) {
+      this.x += this.moveSpeed * forwardX * deltaTime / 1000;
+      this.y += this.moveSpeed * forwardY * deltaTime / 1000;
+    }
+  }
+
+  draw(graphics) {
+    // render player base
+    graphics.save();
+    graphics.translate(this.x, this.y);
+    graphics.rotate(this.forwardRot);
+    graphics.strokePoints(this.baseGeo);
+    graphics.restore();
+
+    // render cannon
+    graphics.save();
+    graphics.translate(this.x, this.y);
+    graphics.rotate(this.cannonRot);
+    graphics.fillCircle(0, 0, 12);
+    graphics.fillRectShape(this.cannonBod);
+    graphics.restore();
+  }
+}
+
+module.exports = Player;
--- src/Game/index.js
+++ src/Game/index.js
@@ -1,7 +1,11 @@
 // Import outside libraries
 const Phaser = require('phaser');
+const Player = require('./Player');
+const Bullet = require('./Bullet');
 // Local Modules
 const SerialPortReader = require('./SerialPortReader');
+
+let shoot;
 
 const config = {
   type: Phaser.AUTO,
@@ -9,20 +13,72 @@ const config = {
   height: 600,
 };
 
+let graphics;
+let keys;
+
+const p1 = new Player(config.width / 2, config.height / 2);
+
+const bullets = [];
+for (let i = 0; i < 20; i ++) {
+  bullets.push(new Bullet());
+}
+
 const serial = new SerialPortReader();
 
 // Phaser setup
 function create() {
-
+  keys = {
+    left: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT),
+    right: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT),
+    up: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP),
+    down: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN),
+    space: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE),
+    a: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+    d: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+    }
+  graphics = this.add.graphics({
+    fillStyle: { color: 0xeeeeee },
+    lineStyle: { width: 3, color: 0xeeeeee }
+  });
 }
 
 function update(totalTime, deltaTime) {
+  p1.update(deltaTime, keys);
 
+  // Keep player on screen
+  if (p1.x > config.width + 15) {
+    p1.setX(0);
+  }
+
+  if (p1.x < -5) {
+    p1.setX(config.width - 15);
+  }
+
+  if (p1.y > config.height + 15) {
+    p1.setY(0);
+  }
+
+  if (p1.y < -5) {
+    p1.setY(config.height - 15);
+  }
+
+  if(shoot == 's'){
+    const newBullet = bullets.find(b => !b.isActive);
+    if (newBullet) newBullet.activate(p1.x, p1.y, p1.cannonRot);
+  }
+  bullets.forEach(b => b.update(deltaTime));
+
+  graphics.clear();
+  p1.draw(graphics);
+  bullets.forEach(b => b.draw(graphics));
 }
 
 function onSerialMessage(msg) {
   // Put your serial reading code in here. msg will be a string
-  console.log(msg);
+  // console.log(msg.split(':'));
+  let vars = msg.split(':');
+  shoot =  vars[0];
+
 }
 
 
